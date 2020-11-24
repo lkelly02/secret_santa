1286d6e7294d30e61d5299922a2600e887de5543
Lucas Kelly <Lucas@Lucass-MacBook-Pro.local>
2020-11-23 22:07:27 -0500

    Update to test.py, need to work on else statement

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
+import sys
 
-# gift_giver = ["Lucas", "Andrea", "Ted", "Sheila", "Jake", "Katlyn", "Brandan", "Kayla"]
-# recipient = [family1, family2, family3, family4]
+# # The idea for the family variables, are that they can be created by the user for as many families and family memebers as they want.
+# # Not sure how to do this yet.
 
 family1 = ["Lucas", "Andrea"]
 family2 = ["Ted", "Sheila"]
@@ -9,25 +10,24 @@ family3 = ["Jake", "Katlyn"]
 family4 = ["Brandan", "Kayla"]
 
 families = [family1, family2, family3, family4]
-recipient = ["Lucas", "Andrea", "Ted", "Sheila", "Jake", "Katlyn", "Brandan", "Kayla"]
+recipients = ["Lucas", "Andrea", "Ted", "Sheila",
+              "Jake", "Katlyn", "Brandan", "Kayla"]
 
 
 chosen_gift_giver = []
 chosen_recipient = []
 
-while len(recipient) > 0 and (len(chosen_gift_giver) and len(chosen_recipient)) < 8:
+while len(recipients) > 0 and (len(chosen_gift_giver) and len(chosen_recipient)) < len(recipients):
     for family in families:
         fam1 = family
         for name in fam1:
             if name not in chosen_gift_giver:
                 rand1 = name
-                rand2 = random.choice(recipient)
+                rand2 = random.choice(recipients)
                 if rand1 != rand2 and rand2 not in fam1:
                     chosen_gift_giver.append(rand1)
                     chosen_recipient.append(rand2)
-                    recipient.remove(rand2)
+                    recipients.remove(rand2)
                     print(f"{rand1} has {rand2}")
                 else:
-                    pass
-
-
+                    sys.exit
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+import random
 
 print("Welcome to the Secret Santa name generator")
 print("Let's start with a few questions:")
@@ -6,20 +6,37 @@ print("Let's start with a few questions:")
 households_or_group = input(
     "Will this game be played with multiple households or one group? ").lower()
 
+chosen_gift_giver = []
+chosen_recipient = []
+
 if households_or_group == "households":
-    list_of_families = []
+    list_of_households = []
     recipients = ""
 
     number_of_households = int(input("How many households are in the game? "))
-    number_in_game = int(
-        input("How many total individuals are playing the game? "))
+    # number_in_game = int(
+    #     input("How many total individuals are playing the game? "))
 
     for num in range(number_of_households):
-        household = input("Enter names of household seperated by commas: ") + " "
-        list_of_families.append(household.split(","))
-        recipients += "".join(household.split(","))
-    recipients = recipients.split()  
+        household = input("Enter names of household seperated by commas: ")
+        list_of_households.append(household.split(", "))
+        recipients += " ".join(household.split(", ")) + " "
+    recipients = recipients.split()
     print(recipients)
+    while len(recipients) > 0 and (len(chosen_gift_giver) and len(chosen_recipient)) < len(recipients):
+        for household in list_of_households:
+            for name in household:
+                if name not in chosen_gift_giver:
+                    rand1 = name
+                    rand2 = random.choice(recipients)
+                    if rand1 != rand2 and rand2 not in household:
+                        chosen_gift_giver.append(rand1)
+                        chosen_recipient.append(rand2)
+                        recipients.remove(rand2)
+                        print(f"{rand1} has {rand2}")
+                    else:
+                        pass
+
 else:
     print("else")
     # number_in_game = int(
